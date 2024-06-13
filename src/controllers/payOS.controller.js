6bce1c248d580ac2a4d515546595b2fbc1608069
PhucPD <dangphuc2644@gmail.com>
2024-06-13 15:31:27 +0700

    update return url

--- src/controllers/payOS.controller.js
+++ src/controllers/payOS.controller.js
@@ -16,8 +16,8 @@ const createPaymentPayosController = catchAsync(async (req, res) => {
     amount,
     description,
     orderCode,
-    returnUrl: 'http://10.10.60.103:9875/success',
-    cancelUrl: 'http://10.10.60.103:9875/error',
+    returnUrl: 'http://10.10.60.103:9876/success',
+    cancelUrl: 'http://10.10.60.103:9876/error',
   };
   try {
     const paymentLink = await payos.createPaymentLink(order);
